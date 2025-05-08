5d925d70b7f7cfe0e3a06e5d9fa4baca1fea6fba
narendra247 <73517605+narendra247@users.noreply.github.com>
2025-05-08 14:56:07 +0530

    Add files via upload

---
+++ TerraformTest/modules/ResourceGroup/outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "Name of the Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "location" {
+  description = "Location of the Resource Group"
+  value       = azurerm_resource_group.rg.location
+}
