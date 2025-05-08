06ef9a110d91b949904c725694154b39da0df404
narendra247 <73517605+narendra247@users.noreply.github.com>
2025-05-08 14:55:32 +0530

    Add files via upload

---
+++ TerraformTest/modules/LogWorkspace/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the Workspace"
+  type        = string
+}
+
+variable "workspace_name" {
+  description = "Name of the  Workspace"
+  type        = string
+}
