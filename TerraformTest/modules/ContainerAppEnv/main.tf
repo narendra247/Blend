06dff0cd717c348afb7a538f31b07f1c075b827b
narendra247 <73517605+narendra247@users.noreply.github.com>
2025-05-08 14:55:05 +0530

    Add files via upload

--- TerraformTest/modules/ContainerAppEnv/main.tf
+++ TerraformTest/modules/ContainerAppEnv/main.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_container_app_environment" "env" {
+  name                       = var.container_app_env_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+}
---
+++ TerraformTest/modules/ContainerAppEnv/outputs.tf
@@ -0,0 +1,4 @@
+output "environment_id" {
+  description = "ID of the Container App Environment"
+  value       = azurerm_container_app_environment.env.id
+}
